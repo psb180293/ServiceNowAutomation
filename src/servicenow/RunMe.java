4c83b799fae4e308b15cd67ecab284c27215d42f
Partha <Partha@LAPTOP-F33MB47C>
2020-03-27 15:52:31 +0530

    commit

---
+++ src/servicenow/RunMe.java
@@ -0,0 +1,22 @@
+package servicenow;
+
+public class RunMe {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		
+		
+		launchServiceNow lsn = new launchServiceNow();
+		
+		String username = "admin";
+		String password= "Jan@2020!";
+		String url="https://dev62656.service-now.com/";
+		String chromepath="E:\\Selenium\\chromedriver_win32\\chromedriver.exe";
+		
+		
+		launchServiceNow.startServiceNow(url, username, password, chromepath);
+		// TODO Auto-generated method stub
+
+	}
+
+}
